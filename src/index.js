29d75036d2c0c65f44558944a2b7cf69f4bec481
manutdmohit <saudmohit@gmail.com>
2021-12-11 11:20:10 +0545

    connecting to mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://manutdmohit:root@cluster0.bfqft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('The connection is successful'))
+  .catch((err) => console.error('There is no connection', err));
 
 app.get('/', (req, res) => {
   res.send('Hi there');
